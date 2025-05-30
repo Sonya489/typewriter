814d65416f007843ba2e833ac86ab2a63480896d
v-lovtsov <v.lovtsov@g.nsu.ru>
2025-05-30 22:52:36 +0700

    Update and rename keyboard_controller.cv to keyboard_controller.sv

--- keyboard_controller.sv
+++ keyboard_controller.sv
@@ -1,27 +1,12 @@
 module keyboard_controller(
-    input  logic clk,                //Тактовый сигнал 32.768 МГц
-    input  logic [3:0] cols_in,      //Столбцы клавиатуры [T13, R13, T14, R14]
-    output logic [3:0] rows_out,     //Строки клавиатуры [P10, R11, T12, R12]
-    output logic [4:0] key_code,     //5-битный код нажатой клавиши
-    output logic key_pressed         //Флаг нажатия клавиши
+    input  logic clk,
+    input  logic [3:0] cols_in,      //[T13, R13, T14, R14]
+    output logic [3:0] rows_out,     //[P10, R11, T12, R12]
+    output logic [4:0] key_code,
+    output logic key_pressed
 );
-
-    //Делитель частоты
-    logic [14:0] counter = 0;
-    logic slow_clk = 0;
-    
-    always_ff @(posedge clk) begin
-        if (counter == 15'd32000) begin
-            slow_clk <= ~slow_clk;
-            counter <= 0;
-        end else begin
-            counter <= counter + 1;
-        end
-    end
-
-    //Сканирование клавиатуры
     logic [1:0] row_idx = 0;
-    always_ff @(posedge slow_clk) begin
+    always_ff @(posedge clk) begin
         row_idx <= row_idx + 1;
     end
 
@@ -30,33 +15,31 @@ module keyboard_controller(
         rows_out[row_idx] = 1'b1;
     end
 
-    //Обнаружение нажатия
-    logic prev_pressed = 0;
     
-    always_ff @(posedge slow_clk) begin
+    always_ff @(posedge clk) begin
         case (cols_in)
-            4'b0001: 
+            4'b0001:
                 case (row_idx)
                     2'b00: key_code <= 5'b00001; // 1
                     2'b01: key_code <= 5'b00010; // 2
                     2'b10: key_code <= 5'b00011; // 3
                     2'b11: key_code <= 5'b01010; // A
                 endcase
-            4'b0010: 
+            4'b0010:
                 case (row_idx)
                     2'b00: key_code <= 5'b00100; // 4
                     2'b01: key_code <= 5'b00101; // 5
                     2'b10: key_code <= 5'b00110; // 6
                     2'b11: key_code <= 5'b01011; // B
                 endcase
-            4'b0100: 
+            4'b0100:
                 case (row_idx)
                     2'b00: key_code <= 5'b00111; // 7
                     2'b01: key_code <= 5'b01000; // 8
                     2'b10: key_code <= 5'b01001; // 9
                     2'b11: key_code <= 5'b01100; // C
                 endcase
-            4'b1000: 
+            4'b1000:
                 case (row_idx)
                     2'b00: key_code <= 5'b01110; // *
                     2'b01: key_code <= 5'b00000; // 0
@@ -66,7 +49,8 @@ module keyboard_controller(
             default: key_code <= key_code;
         endcase
         
-        key_pressed <= (cols_in != 4'b0000);
+         key_pressed <= (cols_in != 4'b0000);
+        
     end
 
 endmodule
