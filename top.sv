0b8177c1ceecb17f9484d56f5bb9f064b82688a3
v-lovtsov <v.lovtsov@g.nsu.ru>
2025-05-30 22:51:53 +0700

    Update top.sv

--- top.sv
+++ top.sv
@@ -5,22 +5,35 @@ module top(
     output logic [31:0] segs_out
 );
 
+    logic [31:0] counter = 0;
+    logic slow_clk = 0;
+    
+    always_ff @(posedge clk) begin
+        if (counter == 32'd700_000) begin
+            slow_clk <= ~slow_clk;
+            counter <= 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+    
+
     logic [4:0] key_code;
     logic key_pressed;
 
     keyboard_controller keyboard_inst(
-        .clk(clk),
+        .clk(slow_clk),
         .cols_in(cols_in),
         .rows_out(rows_out),
         .key_code(key_code),
         .key_pressed(key_pressed)
     );
-
-    segment_display display_inst(
-        .clk(clk),
+    
+    
+    display display_inst(
+        .clk(slow_clk),
         .key_code(key_code),
         .key_pressed(key_pressed),
         .segs_out(segs_out)
     );
-
 endmodule
